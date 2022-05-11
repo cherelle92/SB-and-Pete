84aeff0d2ce46f1948680bc8c82e7c927ca77356
Nagore <nagore.valero@gmail.com>
2022-05-11 12:40:46 +0100

    Front page

--- movieapp/app/assets/config/manifest.js
+++ movieapp/app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+
+
---
+++ movieapp/app/controllers/movie_controller.rb
@@ -0,0 +1,4 @@
+class MovieController < ApplicationController
+  def index
+  end
+end
