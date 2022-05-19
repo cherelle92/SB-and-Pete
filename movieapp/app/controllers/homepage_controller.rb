e14f2cd20b87564ad992decdcc9e1778396f5eac
Nagore <nagore.valero@gmail.com>
2022-05-19 12:17:13 +0100

    display of movies and mood

--- movieapp/app/controllers/homepage_controller.rb
+++ movieapp/app/controllers/homepage_controller.rb
@@ -4,7 +4,14 @@ class HomepageController < ApplicationController
   end
 
   def select
-    @mood_id = params[:id]
-    @mood = params[:mood]
+    @mood_id = params[:mood_id]
+    @mood = Mood.find_by id: @mood_id
+    moodvies = MoodsMovies.where mood_id: @mood_id
+    @movies_list = moodvies.map do |mv| 
+      Movie.find_by id: mv.movie_id
+    end
+
+    # @movies_all = @movies.all
+    # binding.irb
   end
 end
